a1cd20a7d90d447850193b60e94056922eeb2947
dmitriy <dkabardinov@gmail.com>
2016-10-24 00:39:53 +0300

    🚧 breadcrumbs in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, AfterViewInit, ViewChild } from '@angular/core';
+import { CourseDetail} from "./course-detail/course-detail.component";
+import { ActivatedRoute, UrlSegment, RouterState, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 /*
  * App Component
  * Top Level Component
@@ -14,9 +17,13 @@ import { Component, ViewEncapsulation } from '@angular/core';
       <header class="header">
        <div class="header__logo"></div>
        <h1 class="header__text">Logo</h1>
+       <nav class="breadcrumbs">
+        <a  routerLink="/courses" class="breadcrumbs__anchor"  routerLinkActive="active">Курсы</a>
+        <!--<a *ngIf="courseComponent" routerLink="/course/" class="breadcrumbs__anchor" routerLinkActive="active">{{ courseComponent.course.title }}</a>-->
+       </nav>
       </header>
       <main>
-        <router-outlet></router-outlet>
+        <router-outlet (activate)='onActivate($event)'></router-outlet>
       </main>
     </div>
     <footer class="site-footer">
@@ -26,7 +33,20 @@ import { Component, ViewEncapsulation } from '@angular/core';
 })
 export class App {
   name = 'Angular 2 study app';
+  hookedUp = false;
 
-  constructor() {}
+  constructor(public route: ActivatedRoute, router: Router) {
+    router.events.subscribe((e) => console.log(e));
+  }
+
+  courseComponent: CourseDetail = null;
+
+  onActivate(e) {
+    //console.log('route activated', JSON.stringify(e));
+    if  (e instanceof CourseDetail) {
+      this.courseComponent = e;
+      setTimeout(() => console.log(this.courseComponent.course.title), 0);
+    }
+  }
 
 }
